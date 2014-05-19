7c3e5a63710094149249ca2cdfbb26929a675be5
Patt92 <hop@bito.de>
2014-05-19 11:31:09 +0200

    VC#10

---
+++ ProjectX/ProjectX/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ProjectX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbSpielbrett = new System.Windows.Forms.PictureBox();
+            this.Main_Menu_Ticker = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpielbrett)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbSpielbrett
+            // 
+            this.pbSpielbrett.BackColor = System.Drawing.Color.Black;
+            this.pbSpielbrett.Location = new System.Drawing.Point(-2, -1);
+            this.pbSpielbrett.Name = "pbSpielbrett";
+            this.pbSpielbrett.Size = new System.Drawing.Size(720, 720);
+            this.pbSpielbrett.TabIndex = 0;
+            this.pbSpielbrett.TabStop = false;
+            this.pbSpielbrett.Paint += new System.Windows.Forms.PaintEventHandler(this.pbSpielbrett_Paint);
+            this.pbSpielbrett.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbSpielbrett_MouseMove);
+            // 
+            // Main_Menu_Ticker
+            // 
+            this.Main_Menu_Ticker.Tick += new System.EventHandler(this.Main_Menu_Ticker_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 721);
+            this.Controls.Add(this.pbSpielbrett);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpielbrett)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbSpielbrett;
+        private System.Windows.Forms.Timer Main_Menu_Ticker;
+    }
+}
+
---
+++ ProjectX/ProjectX/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectX
+{
+
+    public partial class Form1 : Form
+    {
+        public cbrett brett;
+        private Bitmap hFlaeche;
+
+        private Graphics gFlaeche;
+        private Int32 Length;
+        private Int32 game_state;
+        private Point[] play;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Length = pbSpielbrett.Size.Width;
+            brett = new cbrett(Length);
+            
+            game_state = 0;
+            Main_Menu_Ticker.Start();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void pbSpielbrett_Paint(object sender, PaintEventArgs e)
+        {
+            gFlaeche = this.CreateGraphics();
+            gFlaeche.FillRectangle(Brushes.Blue, 50, (Length / 2) - 75, Length - 100, 150);
+        }
+
+        private void Main_Menu_Ticker_Tick(object sender, EventArgs e) //Das muss überarbeitet werden, hatte bei mir nur so funktioniert
+        {
+            try
+            {
+                /*if (game_state == 0)
+                {
+                    for(Int32 i=0;i<255;i++)
+                    {
+                        start_button(i);
+                    }
+                    Main_Menu_Ticker.Stop();
+                }*/
+                
+                zeichnen();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler: " + ex.Message);
+            }
+
+            Main_Menu_Ticker.Stop();
+        }
+
+        public void start_button(Int32 alpha)
+        {
+            /*SolidBrush button_color = new SolidBrush(Color.FromArgb(alpha,0, 0, 255));
+            gFlaeche = pbSpielbrett.CreateGraphics();
+            gFlaeche.FillRectangle(button_color, 50, (Length/2)-75, Length-100, 150);
+            button_color = new SolidBrush(Color.FromArgb(alpha, 0, 255, 0));
+            gFlaeche.FillPolygon(button_color, play);*/
+        }
+
+        private void pbSpielbrett_MouseMove(object sender, MouseEventArgs e)
+        {
+            Main_Menu_Ticker.Stop();
+        }
+
+
+        public void zeichnen()
+        {
+            gFlaeche = pbSpielbrett.CreateGraphics();
+            for (Int32 i = 0; i < brett.getRange(); i++)
+            {
+                for (Int32 j = 0; j < brett.getRange(); j++)
+                {
+                    gFlaeche.FillRectangle(Brushes.Gray, i *brett.getflen(), j *brett.getflen(), brett.getflen()-5, brett.getflen()-5);
+                    
+                }
+            }
+        }
+
+    }
+    public class cbrett
+    {
+        public Int32[,] Feld;
+
+        private Int32 max_range;        // Status im Game                   
+
+        private const Int32 default_range = 8;
+
+        private Int32 flen;
+
+        public cbrett(Int32 size,Int32 r)
+        {
+            this.max_range = r;
+
+            //Für quadratische Fläche mit Felder Anzahl
+            Feld = new Int32[r, r];
+            flen = size / max_range;
+        }
+
+        public cbrett(Int32 size)
+        {
+            this.max_range = default_range;
+            Feld = new Int32[this.max_range, this.max_range];
+            flen = size / max_range;
+        }
+
+        public Int32 getRange()
+        { return max_range; }
+
+        public Int32 getflen()
+        { return flen; }
+
+    }
+    
+}
